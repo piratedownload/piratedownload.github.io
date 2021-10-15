0398738644847808aa7617ffe598b62be21187d4
alen-B <alenworkworld@gmail.com>
2021-10-15 11:01:47 +0800

    update version

--- docs/js/app.config.js
+++ docs/js/app.config.js
@@ -1,5 +1,5 @@
 window.njconfig={
-    apkurl:"https://dl.testfairy.com/download/74VK2DHP70W2TC1DTBYJZS9ERF9QD96XYY718C9Y92BJHBM9/pirate_2.70.00-testfairy.apk",
+    apkurl:"https://dl.testfairy.com/download/74VK2DSJ74SJTC1D1DXJEN45WY9G878X1P39QT1PPGTW5YPV/pirate_2.80.00-testfairy.apk",
     isourl:"",
     qrdltext:"https://piratedownload.github.io"
 };
