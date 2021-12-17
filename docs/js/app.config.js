949e2bf7750771b5f4ce0a206d4df991a6e1d56f
alen-x <66928150+alen-x@users.noreply.github.com>
2021-12-17 16:50:03 +0800

    Update app.config.js
    update apkurl

--- docs/js/app.config.js
+++ docs/js/app.config.js
@@ -1,5 +1,5 @@
 window.njconfig={
-    apkurl:"https://dl.testfairy.com/download/74VKED9Q68T2TC1DKBH1GKZTPT43TGYWGS9QA1XQ4KQRD1PA/pirate_3.01.00-testfairy.apk",
+    apkurl:"https://dl.testfairy.com/download/74VKEDHJ60TJTC1DXW2G5R1RHFZWYDWY0PDNQTFAB761V5X2/pirate_3.02.00-testfairy.apk",
     isourl:"",
     qrdltext:"https://piratedownload.github.io"
 };
